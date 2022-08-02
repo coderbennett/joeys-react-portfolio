f62560433dfcf5614b1f0f71b5cc284aacdafb20
coderbennett <jbennett@csumb.edu>
2022-08-02 11:08:18 -0700

    add content to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,30 @@ import React, { useState } from 'react';
 
 function Header() {
     return (
-        <div className="navbar bg-base-100">
-            <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">joey bennett</a>
+        <section>
+            <div className="navbar bg-base-100 fixed top-0 w-100 z-50">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl">joey bennett</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal p-0">
+                    <li><a>about</a></li>
+                    <li><a>resume</a></li>
+                    <li><a>projects</a></li>
+                    </ul>
+                </div>
             </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal p-0">
-                <li><a>about</a></li>
-                <li><a>resume</a></li>
-                <li><a>projects</a></li>
-                </ul>
+            
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Welcome!</h1>
+                    <p className="py-6">I'm joey and I am a software developer! I built websites with React, JavaScript, HTML, CSS, MongoDB, Express and Node.js</p>
+                    <button className="btn btn-primary">Learn More</button>
+                    </div>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
